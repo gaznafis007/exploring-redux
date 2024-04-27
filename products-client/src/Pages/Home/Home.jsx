574f02b586caf7209e1fecce10f4280f1de2a7d8
gaznafis007 <gazinafisrafi.gnr@gmail.com>
2024-04-27 21:08:24 +0600

    filter reducer implemented

--- products-client/src/Pages/Home/Home.jsx
+++ products-client/src/Pages/Home/Home.jsx
@@ -35,15 +35,27 @@ const Home = () => {
     dispatch({ type: reduxActionTypes.ADD_TO_CART, payload: product });
   };
 
-  let content = products;
-  console.log(content);
-  if (inStock){
-    content = products.filter(product=>product.status === true)
+  let content;
+
+  if(products.length){
+    content = products.map(product=><Card key={product.model} product={product} actionName={"add to wishlist"} btnColor={"bg-blue-500"} eventHandler={()=>addToWishlist(product)}></Card>)
   }
-  if (brands.length){
-    content = products.filter(product=> brands.includes(product.brand))
-    console.log(content);
+  if(products.length && (inStock || brands.length)){
+    content = products.filter(product=>{
+      if(inStock){
+        return product.status === true
+      }
+      return product
+    })
+    .filter(product=>{
+      if(brands.length){
+        return brands.includes(product.brand)
+      }
+      return product
+    })
+    .map(product=><Card key={product.model} product={product} actionName={"add to wishlist"} btnColor={"bg-blue-500"} eventHandler={()=>addToWishlist(product)}></Card>)
   }
+
 // console.log(content);
     return (
       <div>
@@ -53,10 +65,8 @@ const Home = () => {
             <p onClick={()=>dispatch(action(reduxActionTypes.TOGGLE_BRANDS, "AMD"))} className={`bg-slate-100 rounded-full px-4 py-2 capitalize cursor-pointer hover:bg-slate-200`}>AMD</p>
             <p onClick={()=>dispatch(action(reduxActionTypes.TOGGLE_BRANDS, "Intel"))} className={`bg-slate-100 rounded-full px-4 py-2 capitalize cursor-pointer hover:bg-slate-200`}>intel</p>
           </div>
-          <div className="my-2 grid grid-cols-2 gap-3">
-              {
-                content.map(product=><Card key={product.model} product={product} actionName={"add to wishlist"} btnColor={"bg-blue-500"} eventHandler={()=>addToWishlist(product)}></Card>)
-              }
+          <div className="my-2 grid grid-cols-4 gap-3">
+              {content}
           </div>
       </div>
     )
